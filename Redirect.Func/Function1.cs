9947f03093fc80d14b64efb54c04ce5a30132f1a
luisg <gabrielnino@gmail.com>
2026-01-17 07:24:39 -0800

    Refactor.

--- Redirect.Func/Function1.cs
+++ Redirect.Func/Function1.cs
@@ -1,7 +1,7 @@
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Net;
 
 namespace Redirect.Func
 {
@@ -15,10 +15,15 @@ namespace Redirect.Func
         }
 
         [Function("Function1")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public HttpResponseData Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
         }
     }
 }
